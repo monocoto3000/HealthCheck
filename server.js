760b57537543af654ae0ef2f36488831afaa3f1e
monocoto3000 <128664121+monocoto3000@users.noreply.github.com>
2025-05-14 11:06:39 -0600

    Change file - Server

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(helmet());
 app.use(express.json());
 
 app.get('/health', (_req, res) => {
-  res.status(200).json({ status: 'UP', timestamp: new Date().toISOString(), author: 'Hola soy mon' });
+  res.status(200).json({ status: 'UP', timestamp: new Date().toISOString(), author: 'Mon Moniquito' });
 });
 
 const PORT = process.env.PORT || 3000;
